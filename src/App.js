c16232577ff9c79ab149565ccec3b74359e9b0c8
Giovanni Reid <giovanni-mtl@hotmail.com>
2023-04-23 15:23:45 -0400

    addde mailchimp

--- src/App.js
+++ src/App.js
@@ -1,43 +1,49 @@
-import logo from './logo.svg'
-import './App.css'
-
+import { useEffect } from 'react'
 import axios from 'axios'
+import './App.css'
 
 function App() {
 	const handleSubmit = async (e) => {
-		console.log('submitted')
+		console.log('submitting')
 		e.preventDefault()
-
 		const url =
 			'https://api.beehiiv.com/v2/publications/pub_ef6a608a-e3a4-4cd1-be9a-32c4aa95c1d9/subscriptions'
 
 		const config = {
 			headers: {
+				Accept: 'application/json',
 				'Content-Type': 'application/json',
 				Authorization:
-					'Bearer xRaSvAhf4Uhhloks9PJSTJBzCRAE9hfRhWaOqrwnBZgYUeqI0KSTzhfrqM0OEAUx',
-				'Access-Control-Allow-Origin': 'http://localhost:3000',
-				'Access-Control-Allow-Credentials': 'true',
+					'Bearer ' +
+					'xRaSvAhf4Uhhloks9PJSTJBzCRAE9hfRhWaOqrwnBZgYUeqI0KSTzhfrqM0OEAUx',
 			},
-			withCredentials: true,
 		}
 
 		const rawJson = {
-			email: 'Peru.wayne@wayneenterprise.com',
+			email: 'bruce.wayne@wayneenterprise.com',
 			reactivate_existing: false,
 			send_welcome_email: false,
 			utm_source: 'WayneEnterprise',
 			utm_campaign: 'fall_2022_promotion',
 			utm_medium: 'organic',
 			referring_site: 'www.wayneenterprise.com/blog',
-			custom_fields: [],
+			custom_fields: [
+				{
+					name: 'First Name',
+					value: 'Bruce',
+				},
+				{
+					name: 'Last Name',
+					value: 'Wayne',
+				},
+			],
 		}
 		console.log(rawJson)
 
 		axios
 			.post(url, rawJson, config)
 			.then(function (response) {
-				console.log(response)
+				console.log('Response I Got Back:', response)
 			})
 			.catch(function (error) {
 				console.log(error)
@@ -47,19 +53,8 @@ function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
 				<iframe
+					id='SubscriptionForm'
 					title='This is a unique title'
 					src='https://embeds.beehiiv.com/0890e8d8-e651-43a8-8a83-c25835c05402?slim=true'
 					data-test-id='beehiiv-embed'
@@ -73,6 +68,7 @@ function App() {
 					}}
 				></iframe>
 
+				<div></div>
 				<form onSubmit={handleSubmit}>
 					<input type='text' />
 					<button>Submit</button>
